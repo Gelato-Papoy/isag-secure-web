2d0205ae10f995217ce6251b341395b78084c12d
Rinlapat <rinlapat@proteus-tech.com>
2015-06-28 22:25:30 +0700

    add template dir

--- chater/models.py
+++ chater/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Messages(models.Model):
+    user = models.CharField(max_length=30)
+    talk = models.CharField(max_length=30)
+    message = models.CharField(max_length=300)
+    time = models.DateTimeField(auto_now_add=True)
--- chater/views.py
+++ chater/views.py
@@ -1,22 +1,36 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate
+from django.template import loader, RequestContext
 
-# class UserLogin:
-#     def __init__(self,request):
-#         print "AAAA"
-#         HttpResponse("Hello, world. You're at the polls index.")
-#
 
-def index(request):
+def login(request):
+    # import ipdb;ipdb.set_trace()
+    template = loader.get_template('chater/login.html')
+    context = RequestContext(request, {
+        'aa': "aaaaaaa",
+    })
+    return HttpResponse(template.render(context))
+    #return render(request,)
     # return HttpResponse("Hello, world. You're at the polls index.")
-    user = authenticate(username='admin', password='admin')
-    if user is not None:
-        # the password verified for the user
-        if user.is_active:
-            return HttpResponse("User is valid, active and authenticated")
-        else:
-            return HttpResponse("The password is valid, but the account has been disabled!")
-    else:
-        # the authentication system was unable to verify the username and password
-        return HttpResponse("The username and password were incorrect.")
+    # username = request.POST['username']
+    # password = request.POST['password']
+    # user = authenticate(username='admin', password='admin')
+    # if user is not None:
+    #     # the password verified for the user
+    #     if user.is_active:
+    #         return HttpResponse("User is valid, active and authenticated")
+    #     else:
+    #         return HttpResponse("The password is valid, but the account has been disabled!")
+    # else:
+    #     # the authentication system was unable to verify the username and password
+    #     return HttpResponse("The username and password were incorrect.")
+
+
+# def register(request):
+
+
+#def listMessage(request):
+#    latest_messages_list = Messages.objects.order_by('-pub_date')
+#    output = ', '.join([p.question_text for p in latest_messages_list])
+#    return HttpResponse(output)
